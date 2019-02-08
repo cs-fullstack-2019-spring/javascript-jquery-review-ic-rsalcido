3dc2ecd8f1ae958347e649772bfab04d060eeec0
Ricardo Salcido <rsalcido@liberty.edu>
2019-02-08 16:57:24 -0600

    week5

---
+++ Problem1.js
@@ -0,0 +1,11 @@
+
+
+// Excersize 1 part a.
+console.log( $("p:first").text());
+console.log( $("#first").text());
+console.log( $(".special:first").text() );
+
+/// Excersize 1 part c.
+$("h1").css("color","blue");
+
+///Excersize 2
